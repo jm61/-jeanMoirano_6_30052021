af2ae9923bacfd00ca4d699485c035fa7334a083
jm61 <jean.moirano@gmail.com>
2021-06-11 16:02:51 +0200

    photographer profile and display jpg/mp4

--- dist/js/photographerPage.js
+++ dist/js/photographerPage.js
@@ -1,10 +1,13 @@
 const url = 'dist/data.json';
-const mainContent = document.querySelector('.main__content')
 // get url params from ?id=
 const urlParams = new URLSearchParams(window.location.search)
 const photographerID = urlParams.get('id')
 let photographersList = []
+let photosList = []
+const photos = document.querySelector('.photos')
+let photographerName = ''
 
+// fetch data from backend
 const getData = async () => {
     const res = await fetch(url)
     const data = await res.json()
@@ -12,15 +15,19 @@ const getData = async () => {
     photographersList = data.photographers
     photographersList.forEach(data => {
         if(data.id == photographerID) {
+        // store photographer name for photos dir path
+            photographerName = data.name
+            photographerName = photographerName.split(' ')
+            photographerName = photographerName[0]
         // set document title
             document.title = `${data.name} Page`
         // Constructor class to build profiles
-            let PhotographerData = new Photographer(data)
-            mainContent.appendChild(PhotographerData.createProfile())
+            new Photographer(data)
         }
-        })
+    })
 }
-//getData()
+getData()
+
 // Photographer profile class
 class Photographer {
     constructor(data) {
@@ -32,45 +39,57 @@ class Photographer {
     this.tagline = data.tagline;
     this.price = data.price;
     this.tags = data.tags;
-    
-// DOM Elements creation and CSS
-    this.createProfile = () => {
-       /*  const profile = document.createElement("div");
-        const profileName = document.createElement("h1");
-        const profileDetails = document.createElement("p");
-        const profileTags = document.createElement("ul");
-        const profileContact = document.createElement("button")
-        const profileImage = document.createElement("img")
-    
-        profile.classList.add("profile"); 
-        profileName.classList.add("profile__name"); 
-        profileDetails.classList.add("profile__details"); 
-        profileTags.classList.add("profile__tagList");
-        profileContact.classList.add('profile__contact')
-        profileImage.classList.add('profile__img')
+    this.createProfile()    
+    }
+// DOM selectors
+  createProfile() {
+    const profileName = document.querySelector('.profile__details__name')
+    const profileLocation = document.querySelector('.profile__details__location')
+    const profileTagline = document.querySelector('.profile__details__tagline')
+    const profileImage = document.querySelector('.profile__image')
+    const tagList = document.querySelector('.tagList')
 
-// DOM Generation
-        profileName.innerHTML = `${this.name}`;
-
-        profileDetails.innerHTML = `
-        <strong>${this.city}, ${this.country} </strong> <br>
-        ${this.tagline}<br>`;
-
-        this.tags.forEach(e => {
-            const tag = document.createElement('li')
-            tag.textContent = `#${e}`
-            profileTags.appendChild(tag)
-        })
-
-        profileImage.innerHTML = `<img src="public/images/Photographers/${this.portrait}" class="photographerProfile__img">;`
-
-        
-
-// DOM Display   
-        profile.appendChild(profileName);
-        profile.appendChild(profileDetails);
-        profile.appendChild(profileTags); */
-        return ;
-    };
-  }
+// DOM data injection
+    profileName.textContent = this.name
+    profileLocation.textContent = `${this.city}, ${this.country}`
+    profileTagline.textContent = this.tagline
+    profileImage.src = `public/images/Photographers/${this.portrait}`
+    profileImage.alt = `image de ${this.portrait}`
+    // tag list
+    this.tags.forEach(e => {
+        const tag = document.createElement('li')
+        tag.textContent = `#${e}`
+        tagList.appendChild(tag)
+    }) 
+    return
 }
+}
+    const getPhotos = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log({photographerName})
+    // get list of photos data by photographerId
+    photosList = data.media
+    photosList.forEach(data => {
+        if(data.photographerId == photographerID) {
+            //console.log(data.image)
+            if(data.image == undefined) {
+                const video = document.createElement('video')
+                video.controls = true
+                const source = document.createElement('source')
+                source.src = `public/images/Sample/${photographerName}/${data.video}`
+                source.type = "video/mp4"
+                video.appendChild(source)
+                photos.appendChild(video)
+            } else {
+                const element = document.createElement('img')
+                element.src = `public/images/Sample/${photographerName}/${data.image}`
+                photos.appendChild(element)
+            }
+        }
+    })
+}
+getPhotos()
+
+
+
