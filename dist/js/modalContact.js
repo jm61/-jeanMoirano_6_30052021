208499f06b453f54ca570ebb9ac64dd460a87fa3
jm61 <jean.moirano@gmail.com>
2021-06-15 15:47:58 +0200

    fixed modal

---
+++ dist/js/modalContact.js
@@ -0,0 +1,124 @@
+// DOM Elements Selection
+const modal = document.querySelector(".modal");
+const modalForm = document.querySelector(".modalForm");
+const modalBtn = document.querySelectorAll(".profile__contact");
+const form = document.querySelector("#form");
+const close = document.querySelector('.fa-times')
+const content = document.querySelector('.main')
+// Variables
+const red = "#eee"
+// check RegEx
+const regexEmail = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
+const regexChars = /^[a-zA-Z\u00C0-\u00FF ]+$/
+
+// launch modal event
+modalBtn.forEach(btn => btn.addEventListener("click", () => {
+  modal.style.display = "block"
+  modalForm.style.display = "block"
+  modalForm.focus()
+})
+)
+// close modal form with X
+close.addEventListener('click', () => {
+  modalForm.style.display = "none"
+  modal.style.display = "none"
+})
+
+// Validation First Name
+function validateFirstName() {
+  if(checkIfEmpty(firstName)) return
+  if(!checkOnlyLetters(firstName)) return
+  if(!check2Letters(firstName)) return
+  return true;
+}
+// Validation Last Name
+function validateLastName() {
+  if(checkIfEmpty(lastName)) return
+  if(!checkOnlyLetters(lastName)) return
+  if(!check2Letters(lastName)) return
+  return true
+}
+// Validation Email
+function validateEmail(){
+  if(checkIfEmpty(email)) return
+  if(!checkRegex(email)) return
+  return true
+}
+// Validation Message
+function validateMessage() {
+  if(checkIfEmpty(message)) return
+  return true
+}
+// -------------------------------------------- //
+
+// Input empty check
+function checkIfEmpty(field){
+  if(isEmpty(field.value.trim())) {
+       setInvalid(field, `le champ ${field.name} ne peut pas être vide`)
+      return true
+  } else {
+      setValid(field)
+      return false
+  }
+}
+// Input length check
+function check2Letters(field){
+  if(field.value.length < 2){
+    setInvalid(field, `le champ ${field.name} comprend au moins 2 caractères`)
+    return false
+  } else {
+    setValid(field)
+    return true
+  }
+}
+// Input only letters check
+function checkOnlyLetters(field) {
+  if(regexChars.test(field.value)) {
+      setValid(field)
+      return true
+  } else {
+      setInvalid(field, `le champ ${field.name} comprend seulement des lettres`)
+      return false
+  }
+}
+// Email input check
+function checkRegex(field, message){
+  if(field.value.match(regexEmail)) {
+    setValid(field)
+    return true
+  } else {
+    message = `le champ ${field.name} doit être un email valide`
+    setInvalid(field, message)
+    return false
+  }
+}
+// Input empty check
+function isEmpty(value){
+  if(value === "") return true
+  return false
+}
+
+// Utilities Valid/Invalid
+function setInvalid(field, message){
+  field.nextElementSibling.innerHTML = message
+  field.nextElementSibling.style.color = red
+}
+function setValid(field){
+  field.nextElementSibling.innerHTML = ''
+}
+
+// Handle Form for submit
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  if(validateFirstName() && validateLastName() && validateEmail() && validateMessage()) {
+    //success.style ="display:flex;"
+    modalForm.style="display:none;"
+    modal.style.display = "none"
+    // Dump fields form
+    for(i=0; i<form.elements.length; i++) {
+      console.log(form.elements[i].value)
+    }
+    form.reset()
+  }
+})
--- dist/js/photographerPage.js
+++ dist/js/photographerPage.js
@@ -3,9 +3,12 @@ const url = 'dist/data.json';
 const urlParams = new URLSearchParams(window.location.search)
 const photographerID = urlParams.get('id')
 let photographersList = []
-let photosList = []
-const photos = document.querySelector('.photos')
+//let mediaList = []
+const photosGallery = document.querySelector('.photosGallery')
+const modalTitle = document.querySelector('.modalForm__body__title')
 let photographerName = ''
+let photographerPrice = 0
+let total = []
 
 // fetch data from backend
 const getData = async () => {
@@ -15,13 +18,15 @@ const getData = async () => {
     photographersList = data.photographers
     photographersList.forEach(data => {
         if(data.id == photographerID) {
-        // store photographer name for photos dir path
+        // store photographer price and name for photos dir path
             photographerName = data.name
+            modalTitle.innerHTML = `Contactez-moi<br>${photographerName}`
+            photographerPrice = data.price
             photographerName = photographerName.split(' ')
             photographerName = photographerName[0]
         // set document title
             document.title = `${data.name} Page`
-        // Constructor class to build profiles
+        // Constructor class to build profile
             new Photographer(data)
         }
     })
@@ -60,36 +65,118 @@ class Photographer {
         const tag = document.createElement('li')
         tag.textContent = `#${e}`
         tagList.appendChild(tag)
-    }) 
-    return
+        }) 
+        return
+    }
 }
-}
+// Photos Gallery section
     const getPhotos = async () => {
+    let mediaData
+    //let total = []
     const res = await fetch(url)
     const data = await res.json()
     console.log({photographerName})
     // get list of photos data by photographerId
-    photosList = data.media
-    photosList.forEach(data => {
-        if(data.photographerId == photographerID) {
-            //console.log(data.image)
-            if(data.image == undefined) {
-                const video = document.createElement('video')
-                video.controls = true
-                const source = document.createElement('source')
-                source.src = `public/images/Sample/${photographerName}/${data.video}`
-                source.type = "video/mp4"
-                video.appendChild(source)
-                photos.appendChild(video)
-            } else {
-                const element = document.createElement('img')
-                element.src = `public/images/Sample/${photographerName}/${data.image}`
-                photos.appendChild(element)
-            }
+    mediaData = data.media
+    mediaData.forEach(media => {
+        if(media.photographerId == photographerID) {
+            photosGallery.appendChild(CreateMediaCard(media))
+            total.push(media.likes)
         }
     })
+    // DOM footer setup
+    const likesTotal = document.querySelector('.footer__likesTotal')
+    likesTotal.innerHTML = `${total.reduce((acc,el) => acc+el,0)} <i class="fas fa-heart">`
+    // set total for footer update
+    total = total.reduce((acc,el) => acc+el,0)
+    const price = document.querySelector('.footer__price')
+    price.innerHTML = `${photographerPrice}€ / jour`
 }
 getPhotos()
 
+// Class constructor
+// ----------------------------------------------------------------------- //
+
+const CreateMediaCard = (media) => {
+    let cardObj = new mediaCardParts("card", media)
+    let descObj = new mediaCardParts("desc", media)
+    let mediaObj
+    if(media.image == undefined){
+        mediaObj = new mediaCardParts("video", media)
+    }
+    else{
+        mediaObj  = new mediaCardParts("image", media)
+    }
+    cardObj.mediaCard.appendChild(mediaObj.mediaMedia)
+    cardObj.mediaCard.appendChild(descObj.mediaDesc)
+
+    return cardObj.mediaCard
+}
+// Intermediary between the actual factories classes and the user
+class mediaCardParts{
+    constructor(type, mediaData){
+        if(type === "card"){
+            return new MediaCard()
+        }
+
+        if(type === "desc"){
+            return new MediaFactory_desc(mediaData)
+        }
+
+        if(type === "video"){
+            return new MediaVideo(mediaData)
+        }
+
+        if(type === "image"){
+            return new MediaImage(mediaData)
+        }
+    }
+}
+
+// Various factories used to build a mediaCard
+class MediaCard{
+    constructor(){
+        this.mediaCard = document.createElement("div")
+        this.mediaCard.classList.add("mediaCard")
+    }
+}
+class MediaVideo{
+    constructor(mediaData){
+        this.mediaMedia = document.createElement("div")
+        // this.mediaMedia.classList.add("mediaCard__image") 
+        this.mediaMedia.classList.add("modalMedia_open")
+        this.mediaMedia.innerHTML = `<a href="#" > <video alt="${mediaData.title}" controls> <source src="public/images/Sample/${photographerName}/${mediaData.video}" type="video/mp4">${mediaData.title}, closeup view </video> </a>`
+    }
+}
+class MediaImage{
+    constructor(mediaData){
+        this.mediaMedia = document.createElement("div")
+        //this.mediaMedia.classList.add("mediaCard__image") 
+        this.mediaMedia.classList.add("modalMedia_open")
+        this.mediaMedia.innerHTML = `<a href="#" > <img src="public/images/Sample/${photographerName}/${mediaData.image}" alt="${mediaData.title}, closeup view"></a>`
+    }
+}
+class MediaFactory_desc{
+    constructor(mediaData){
+        this.mediaDesc = document.createElement("div")
+        this.mediaName = document.createElement("p")
+        this.mediaLike = document.createElement("p")
+
+        this.mediaDesc.classList.add("mediaCard__desc") 
+        this.mediaName.classList.add("mediaCard__desc__name") 
+        this.mediaLike.classList.add("mediaCard__desc__number") 
+        this.mediaLike.classList.add("add_like_button") 
+
+        this.mediaName.innerHTML = mediaData.title
+        this.mediaLike.innerHTML = `${mediaData.likes} <i class="fas fa-heart" aria-label="likes"></i>`
+        this.mediaLike.addEventListener('click', () => {
+            this.mediaLike.innerHTML = `${++mediaData.likes} <i class="fas fa-heart" aria-label="likes"></i>`
+            // dynamic update of total likes footer
+            document.querySelector('.footer__likesTotal').innerHTML = `${++total} <i class="fas fa-heart">`
+        })
+        this.mediaDesc.appendChild(this.mediaName)
+        this.mediaDesc.appendChild(this.mediaLike)
+    }
+}
 
 
